9a8154e32e80cc372f4a9a3b2ac32a659c42f7fc
Marcus Augusto <77218852+madogap@users.noreply.github.com>
2022-10-18 16:52:10 -0300

    Update index.js

--- dom/assest/index.js
+++ dom/assest/index.js
@@ -1,12 +1,11 @@
 /*Event DOM MOUSE
 Global variable with reference to the ID in the HTML assigning to a constant areaV where through these we will do what we want. ID (area) HTML => CONST areaV 
 Variável global com referência ao ID no HTML atribuindo a uma constante areaV onde atráves destas faremos o que desejarmos. ID (area) HTML => CONST areaV
-const areaV = window.document.getElementById('area');
 Shoot events whit arrow functions:
 The addEventListener method is an event handler where it received the mouse event and calls its Arrow function to do something.
 Atire eventos com funções de seta:
 O método addEventListener é um manipulador de enventos onde recebeu o evento do mouse e que chama sua Arrow function para que faça algo.*/
-
+const areaV = window.document.getElementById('area');
 areaV.addEventListener('mousedown', (toclick) => {
     areaV.innerText = 'Clicked';
     areaV.style.background = 'red';
